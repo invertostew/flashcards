e3f22c168f9ccc7880392c79fdcdd498d640b9a3
Stuart Green <github.47a8z@aleeas.com>
2021-12-26 21:18:10 +0000

    test constructor function properties

--- __tests__/deck.test.js
+++ __tests__/deck.test.js
@@ -7,4 +7,12 @@ describe('Deck', () => {
         const deck = new Deck();
         expect(deck).toBeInstanceOf(Deck);
     });
+    test('Returns the flashcards property', () => {
+        const deck = new Deck('flashcards');
+        expect(deck.flashcards).toBe('flashcards');
+    });
+    test('Returns the _currentCard property', () => {
+        const deck = new Deck();
+        expect(deck._currentCard).toBe(null);
+    });
 });
--- __tests__/flashcard.test.js
+++ __tests__/flashcard.test.js
@@ -7,4 +7,16 @@ describe('Flashcard', () => {
         const flashcard = new Flashcard();
         expect(flashcard).toBeInstanceOf(Flashcard);
     });
+    test('Returns the concept property', () => {
+        const flashcard = new Flashcard('Concept 1');
+        expect(flashcard.concept).toBe('Concept 1');
+    });
+    test('Returns the definition property', () => {
+        const flashcard = new Flashcard('Concept 1', 'Definition 1');
+        expect(flashcard.definition).toBe('Definition 1');
+    });
+    test('Returns the category property', () => {
+        const flashcard = new Flashcard('Concept 1', 'Definition 1', 'JavaScript');
+        expect(flashcard.category).toBe('JavaScript');
+    });
 });
