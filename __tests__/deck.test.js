1b1937ea41e0c4a7196f158a93ff27f012656b9d
Stuart Green <github.47a8z@aleeas.com>
2021-12-26 22:24:14 +0000

    added drawFlashcard/_removeFlashcard methods

--- __tests__/deck.test.js
+++ __tests__/deck.test.js
@@ -11,8 +11,40 @@ describe('Deck', () => {
         const deck = new Deck('flashcards');
         expect(deck.flashcards).toBe('flashcards');
     });
-    test('Returns the _currentCard property', () => {
+    test('Returns the _currentFlashcard property', () => {
         const deck = new Deck();
-        expect(deck._currentCard).toBe(null);
+        expect(deck._currentFlashcard).toBe(null);
+    });
+    test('Returns the _removedFlashcards property', () => {
+        const deck = new Deck();
+        expect(deck._removedFlashcards).toEqual([]);
+    });
+});
+
+describe('drawFlashcard', () => {
+    let deck, flashcards;
+    beforeEach(() => {
+        flashcards = [
+            {
+                concept: 'Concept 1',
+                definition: 'Definition 1',
+                category: 'Category 1'
+            },
+            {
+                concept: 'Concept 2',
+                definition: 'Definition 2',
+                category: 'Category 2'
+            }
+        ];
+        deck = new Deck(flashcards);
+    });
+    test('Updates the _currentFlashcard property', () => {
+        expect(deck._currentFlashcard).toBe(null);
+        deck.drawFlashcard();
+        expect(deck._currentFlashcard).not.toBe(null);
+    });
+    test('Removes the drawn card from the flashcards array', () => {
+        deck.drawFlashcard();
+        expect(deck._removedFlashcards).toEqual([deck._currentFlashcard]);
     });
 });
--- src/deck.js
+++ src/deck.js
@@ -2,7 +2,25 @@
 
 function Deck(flashcards) {
     this.flashcards = flashcards;
-    this._currentCard = null;
+    this._currentFlashcard = null;
+    this._removedFlashcards = [];
+}
+
+Deck.prototype.drawFlashcard = function () {
+    const index = Math.floor(Math.random() * this.flashcards.length);
+    const flashcard = this.flashcards[index];
+
+    this._currentFlashcard = flashcard;
+    this._removeFlashcard();
+}
+
+Deck.prototype._removeFlashcard = function () {
+    const currentFlashcardIndex = this.flashcards.findIndex((flashcard) => {
+        return flashcard === this._currentFlashcard;
+    });
+    const removedFlashcard = this.flashcards.splice(currentFlashcardIndex, 1);
+    
+    this._removedFlashcards.push(removedFlashcard[0]);
 }
 
 module.exports = Deck;
