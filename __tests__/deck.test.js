a7d75da8085b06dd92feec19fd9ca2d841ae6e47
Stuart Green <github.47a8z@aleeas.com>
2021-12-26 22:29:59 +0000

    tidied up deck tests

--- __tests__/deck.test.js
+++ __tests__/deck.test.js
@@ -2,42 +2,39 @@
 
 const Deck = require('../src/deck.js');
 
+let deck, flashcards;
+beforeEach(() => {
+    flashcards = [
+        {
+            concept: 'Concept 1',
+            definition: 'Definition 1',
+            category: 'Category 1'
+        },
+        {
+            concept: 'Concept 2',
+            definition: 'Definition 2',
+            category: 'Category 2'
+        }
+    ];
+    deck = new Deck(flashcards);
+});
+
 describe('Deck', () => {
     test('Returns an instance of Deck', () => {
-        const deck = new Deck();
         expect(deck).toBeInstanceOf(Deck);
     });
     test('Returns the flashcards property', () => {
-        const deck = new Deck('flashcards');
-        expect(deck.flashcards).toBe('flashcards');
+        expect(deck.flashcards).toEqual(flashcards);
     });
     test('Returns the _currentFlashcard property', () => {
-        const deck = new Deck();
         expect(deck._currentFlashcard).toBe(null);
     });
     test('Returns the _removedFlashcards property', () => {
-        const deck = new Deck();
         expect(deck._removedFlashcards).toEqual([]);
     });
 });
 
 describe('drawFlashcard', () => {
-    let deck, flashcards;
-    beforeEach(() => {
-        flashcards = [
-            {
-                concept: 'Concept 1',
-                definition: 'Definition 1',
-                category: 'Category 1'
-            },
-            {
-                concept: 'Concept 2',
-                definition: 'Definition 2',
-                category: 'Category 2'
-            }
-        ];
-        deck = new Deck(flashcards);
-    });
     test('Updates the _currentFlashcard property', () => {
         expect(deck._currentFlashcard).toBe(null);
         deck.drawFlashcard();
