b4c6e03a3ce42400813171927e052bb8fbfb95b9
Stuart Green <github.47a8z@aleeas.com>
2021-12-27 01:27:08 +0000

    added showAllExamples method

--- __tests__/flashcard.test.js
+++ __tests__/flashcard.test.js
@@ -5,8 +5,13 @@ const Flashcard = require('../src/flashcard.js');
 let flashcards;
 beforeEach(() => {
     flashcards = [
-        new Flashcard('Concept 1', 'Definition 1', 'Category 1'),
-        new Flashcard('Concept 2', 'Definition 2', 'Category 2')
+        new Flashcard('Concept 1', 'Definition 1', 'Category 1', [
+            'Example 1'
+        ]),
+        new Flashcard('Concept 2', 'Definition 2', 'Category 2', [
+            'Example 1',
+            'Example 2'
+        ])
     ];
 });
 
@@ -26,5 +31,18 @@ describe('Flashcard', () => {
     test('Returns the category property', () => {
         expect(flashcards[0].category).toBe('Category 1');
         expect(flashcards[1].category).toBe('Category 2');
+    });
+    test('Returns the examples property', () => {
+        expect(flashcards[0]._examples).toEqual(['Example 1']);
+        expect(flashcards[1]._examples).toEqual(['Example 1', 'Example 2']);
+    });
+});
+
+describe('showAllExamples', () => {
+    test('Returns a formatted string of examples', () => {
+        const examples = flashcards[0].showAllExamples();
+        expect(examples).toEqual(`Here are the examples for ${flashcards[0].concept}!\n\nExample 1:\nExample 1`);
+        const twoExamples = flashcards[1].showAllExamples();
+        expect(twoExamples).toEqual(`Here are the examples for ${flashcards[1].concept}!\n\nExample 1:\nExample 1\n\nExample 2:\nExample 2`);
     });
 });
--- src/flashcard.js
+++ src/flashcard.js
@@ -1,9 +1,19 @@
 'use strict';
 
-function Flashcard(concept, definition, category) {
+function Flashcard(concept, definition, category, _examples) {
     this.concept = concept;
     this.definition = definition;
     this.category = category;
+    this._examples = _examples;
+}
+
+Flashcard.prototype.showAllExamples = function () {
+    const examplesIntro = `Here are the examples for ${this.concept}!`;
+    const examples = this._examples.reduce((accumulator, example, index) => {
+        if (index === 0) return `Example ${index + 1}:\n${example}`;
+        return `${accumulator}\n\nExample ${index + 1}:\n${example}`;
+    }, '');
+    return `${examplesIntro}\n\n${examples}`;
 }
 
 module.exports = Flashcard;
