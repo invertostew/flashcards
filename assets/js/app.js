fc967cd0a45b5f0352b25f61c2034d53e24c7259
Stuart Green <github.47a8z@aleeas.com>
2021-12-28 18:48:04 +0000

    controls and listeners set up for Deck methods

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,52 @@
+// Set up
 const flashcards = [
-    new Flashcard('Concept 1', 'Definition 1', 'Category', [
+    new Flashcard('Concept 1', 'Definition 1', 'Category 1', [
         'Example 1'
     ]),
-    new Flashcard('Concept 2', 'Definition 2', 'Category', [
+    new Flashcard('Concept 2', 'Definition 2', 'Category 2', [
+        'Example 2'
+    ]),
+    new Flashcard('Concept 3', 'Definition 3', 'Category 1', [
+        'Example 1',
         'Example 2'
     ])
 ];
-
 const deck = new Deck(flashcards);
 
-console.log(deck);
+// App
+const elements = {
+    flashcard: {
+        concept: document.querySelector('#concept'),
+        definition: document.querySelector('#definition')
+    },
+    controls: {
+        drawFlashcard: document.querySelector('#draw-flashcard'),
+        skipFlashcard: document.querySelector('#skip-flashcard'),
+        resetDeck: document.querySelector('#reset-deck')
+    }
+}
+
+const setFlashcardContent = () => {
+    if (deck._currentFlashcard) {
+        elements.flashcard.concept.textContent = deck._currentFlashcard.concept;
+        elements.flashcard.definition.textContent = deck._currentFlashcard.definition;
+    } else {
+        elements.flashcard.concept.textContent = 'Draw a Flashcard';
+        elements.flashcard.definition.textContent = 'Please press the "Draw Flashcard" button.';
+    }
+}
+
+elements.controls.drawFlashcard.addEventListener('click', () => {
+    deck.drawFlashcard();
+    setFlashcardContent();
+});
+
+elements.controls.skipFlashcard.addEventListener('click', () => {
+    deck.skipFlashcard();
+    setFlashcardContent();
+});
+
+elements.controls.resetDeck.addEventListener('click', () => {
+    deck.resetDeck();
+    setFlashcardContent();
+});
